00cc08f54bd664e1a2b81739edb796061c0d1bb7
skparkes <bias.variance@gmail.com>
2015-01-22 22:25:12 -0500

    Read in and scrub the data nicely.
    Do this to prepare ourselves for modeling.

--- 01_Modeling.R
+++ 01_Modeling.R
@@ -8,9 +8,85 @@ opts_chunk$set(error=FALSE)
 
 #' ## Course Project for [Practical Machine Learning](https://www.coursera.org/course/predmachlearn)
 #'   * by Shea Parkes
-#'
+
+library(magrittr)
+library(dplyr)
+library(randomForestSRC)
 #' ----
 
+#' ### Read in the data into R nicely
+df.all <- lapply(
+  c('training', 'testing')
+  ,. %>%{
+    i.df <- read.csv(
+      paste0('pml-',.,'.csv')
+      ,na.strings = c('NA','#DIV/0!','')
+      ,stringsAsFactors = FALSE
+      )
+    i.df$X <- NULL
+    if(. == 'testing'){
+      i.problems <- i.df$problem_id
+      i.df$problem_id <- NULL
+      i.df$classe <- as.character(NA)
+      i.df$train <- FALSE
+      i.df$problem_id <- i.problems
+    } else {
+      i.df$train <- TRUE
+      i.df$problem_id <- as.integer(NA)
+    }
+    i.df
+  }) %>%
+  {do.call(rbind, .)} %>%
+  {rownames(.) <- NULL; .}
+
+df.all %>% str(list.len=length(.))
+
+#' Convert to factor after stacking so levels will be consistent
+df.all %<>% sapply(is.character) %>%
+  {
+    df.all[.] <- lapply(df.all[.], as.factor)
+    print(str(df.all[.]))
+    df.all
+  }
+
+#' Move the non-feature data out into isolated vectors
+classe <- df.all$classe
+df.all$classe <- NULL
+
+problem_id <- df.all$problem_id
+df.all$problem_id <- NULL
+
+ind.train <- df.all$train
+df.all$train <- NULL
+
+
+#' ### Inspect the blatantly obvious relationships
+table(df.all$user_name, df.all$cvtd_timestamp, ind.train)
+table(df.all$cvtd_timestamp, classe, useNA = 'ifany')
+table(df.all$user_name, classe, useNA = 'ifany')
+
+
+#' ### Isolate and remove the almost-never-populated features
+pct.na <- df.all %>%
+  sapply(. %>% is.na() %>% mean()) %T>%
+  {sort(.) %>% dotchart()} %T>%
+  {sort(.) %>% tail(., 20) %>% print()}
+
+df.all %<>% '['(pct.na < 0.8)
+
+#' ### Do a simple RandomForest fit
+myfit <- rfsrc(
+  classe~.
+  ,data=data.frame(classe,df.all)[ind.train,]
+  ,ntree=50
+  ,do.trace=TRUE
+  ,nsplit=12
+  )
+
+print(myfit)
+plot(myfit)
+
+predict(myfit,newdata=df.all[!ind.train,])$class
 
 #' ### Session information
 sessionInfo()
@@ -18,4 +94,5 @@ sessionInfo()
 #' ### Bibliography
 #+ bibliography, echo=FALSE, results='asis'
 print(citation(),style='html')
-print(citation('randomForest'),style='html')
+print(citation('magrittr'),style='html')
+print(citation('randomForestSRC'),style='html')
